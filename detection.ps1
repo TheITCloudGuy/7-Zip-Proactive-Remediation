560d17e3c7e2a6a91e3203e136562707a88e9b92
Stephen Devlin <devvbot2@gmail.com>
2022-04-22 09:45:13 +0100

    Proactive Remediation

---
+++ detection.ps1
@@ -0,0 +1,20 @@
+﻿try
+{  
+
+$7ZipInstalled = Test-Path 'C:\Program Files\7-Zip\7-zip.chm'
+
+if ($7ZipInstalled -eq 'True') {
+    Write-Host "7-Zip Help file present."
+    exit 1
+    }
+    else {
+        #No remediation required    
+        Write-Host "7-Zip Help file NOT present."
+        exit 0
+    }  
+}
+catch {
+    $errMsg = $_.Exception.Message
+    Write-Error $errMsg
+    exit 1
+}
---
+++ remediation.ps1
@@ -0,0 +1,11 @@
+﻿$FileLocation = "C:\Program Files\7-Zip\7-zip.chm"
+$FileName = "7-zip.chm"
+
+if (Test-Path $FileLocation ) {
+  Remove-Item $FileLocation 
+  write-host "$FileName has been deleted"
+}
+
+else {
+  Write-host "$FileName doesn't exist"
+}
